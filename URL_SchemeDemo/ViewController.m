18c4089108c9a5ae41aed0dc2cf2cdf7f51480c4
alen <jianzhuangwang@jianzhuangwangdeiMac.local>
2016-06-17 14:55:53 +0800

    自定义URLScheme

---
+++ URL_SchemeDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  URL_SchemeDemo
+//
+//  Created by 建装网 on 16/6/17.
+//  Copyright © 2016年 Alen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
